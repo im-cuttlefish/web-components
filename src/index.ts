59dc033101f76fdd76f2994817fa4caf9adc3784
im-cuttlefish <firefly.squid.ink@gmail.com>
2019-02-26 00:22:46 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@ import { preview } from "./preview";
 import { heroHeader } from "./web-components";
 
 const json = {
-  html: '<hero-header><span slot="title">aaaa</span></hero-header>',
+  html: `
+    <hero-header>
+      <span slot="title">aaaa</span>
+      <img slot="cover" src="./assets/cover.jpeg">
+    </hero-header>
+  `,
   components: [heroHeader]
 };
 
 document.body.appendChild(preview);
 setTimeout(() => {
-  preview.contentWindow.postMessage(JSON.stringify(json), "*");
+  preview.contentWindow!.postMessage(JSON.stringify(json), "*");
 }, 1000);
---
+++ src/preview/preview.ts
@@ -0,0 +1,6 @@
+import { Component } from "../web-components/component";
+
+export interface IPreview {
+  html: string;
+  components: Component[];
+}
